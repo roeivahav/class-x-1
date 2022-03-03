5769ada2b26ff4c476ccf965003b267325ff024a
EldorZandani <fox18966@gmail.com>
2022-03-03 15:46:09 +0200

    how to make more then one class

---
+++ class x 1/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_x_1
+{
+    class Ball
+    {
+        int size;
+        string color;
+        string type;
+        string fabric;
+
+        public int get_size()
+        {
+            return this.size;
+        }
+        public void set_size(int size)
+        {
+            this.size = size;
+        }
+        public string get_color()
+        {
+            return this.color;
+        }
+        public void set_color(string color)
+        {
+            this.color = color;
+        }
+        public string get_type()
+        {
+            return this.type;
+        }
+        public void set_type(string type)
+        {
+            this.type = type;
+        }
+        public string get_fabric()
+        {
+            return this.fabric;
+        }
+        public void set_fabric(string fabric)
+        {
+            this.fabric = fabric;
+        }
+
+    }
+
+}
---
+++ class x 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_x_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your ball details:");
+            Ball my_new_ball = new Ball();
+            Console.WriteLine("size?");
+            my_new_ball.set_size(int.Parse(Console.ReadLine()));
+            Console.WriteLine("color?");
+            my_new_ball.set_color(Console.ReadLine());
+            Console.WriteLine("type?");
+            my_new_ball.set_type(Console.ReadLine());
+            Console.WriteLine("fabric?");
+            my_new_ball.set_fabric(Console.ReadLine());
+
+
+
+
+
+
+
+        }
+    }
+}
